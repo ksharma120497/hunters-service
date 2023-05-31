89042cb7769e1fa7b0e188269fa99e041f8d6433
Kapil Sharma <ksharma120497@gmail.com>
2023-06-01 01:13:55 +0530

    First commit

---
+++ src/main/java/com/lsk/huntersservice/HuntersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lsk.huntersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuntersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuntersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsk/huntersservice/HuntersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsk.huntersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuntersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
